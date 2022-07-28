270c297ee414e44f1ca18531c920ce620f5fd5a1
YeyoM <yeyomoreno2003@hotmail.com>
2022-07-28 18:51:31 -0500

    minor changes

--- src/components/AboutMeContainer/AboutMeContainer.jsx
+++ src/components/AboutMeContainer/AboutMeContainer.jsx
@@ -5,7 +5,7 @@ const AboutMeContainer = () => {
     <div className={classes.aboutMeContainer}>
       <div className={classes.aboutText}>
         <p className={classes.aboutTitle}>Hey there! </p>
-          <p className={classes.aboutP}>My name is Diego and I’m a software engineer focused on web development. I started coding back in 2019 when I was in high school. I’m currently coursing 2nd semester in college. I’m studying Computer Science and my favorite course is “Computational Structures” where I master my code abilities with C++.</p>
+          <p className={classes.aboutP}>My name is Diego and I’m a software engineer student focused on web development. I started coding back in 2019 when I was in high school. I’m currently coursing 2nd semester in college. I’m studying Computer Science and my favorite course is “Computational Structures” where I master my code abilities with C++.</p>
           <p className={classes.aboutP}>By now, I’m looking for my first Internship or my first approach with the IT work field. My main tech core is based mostly in JavaScript and all around with web development and I’m passionate about building great and excellent digital products that live on the internet.</p>
           <p className={classes.aboutP}>Apart from coding, you will normally find me playing basketball or maybe playing guitar, who knows? </p>
         <p className={classes.aboutTech}>Technologies I've used in recent projects:</p>
